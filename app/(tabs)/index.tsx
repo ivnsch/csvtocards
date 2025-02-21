1e719440322e425c8620f0a9759e56f2684240ff
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-21 13:30:44 +0100

    upload and parse csv file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,56 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 import PagerView from "react-native-pager-view";
+import * as DocumentPicker from "expo-document-picker";
+import * as Papa from "papaparse";
+
+type CsvRow = {
+  [key: string]: string; // Each row is an object with string keys and values
+};
 
 export default function HomeScreen() {
+  const [data, setData] = useState<CsvRow[]>([]);
+
+  const pickCSVFile = async () => {
+    try {
+      const result = await DocumentPicker.getDocumentAsync({
+        type: "text/csv",
+      });
+
+      if (result.canceled) return;
+
+      const fileUri = result.assets[0].uri;
+      const response = await fetch(fileUri);
+      const text = await response.text();
+
+      const parsed = Papa.parse<CsvRow>(text, { header: true });
+      console.log("parsed: " + JSON.stringify(parsed));
+
+      setData(parsed.data);
+    } catch (error) {
+      console.error("Error picking CSV file:", error);
+    }
+  };
+
   return (
-    <PagerView style={styles.pagerView} initialPage={0}>
-      <View key="1">
-        <Text style={styles.text}>First page</Text>
-      </View>
-      <View key="2">
-        <Text style={styles.text}>Second page</Text>
-      </View>
-    </PagerView>
+    <View style={styles.container}>
+      <Button title="Pick CSV File" onPress={pickCSVFile} />
+      <PagerView style={styles.pagerView} initialPage={0}>
+        <View key="1">
+          <Text style={styles.text}>First page</Text>
+        </View>
+        <View key="2">
+          <Text style={styles.text}>Second page</Text>
+        </View>
+      </PagerView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   pagerView: {
     flex: 1,
   },
